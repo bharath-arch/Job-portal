0a75c52e53cd52aabd85c6437797bf5f5feab147
bharathkumar <bharathkumar100q@gmail.com>
2024-07-29 00:22:30 +0530

    backend started

--- src/components/Admin/layout/AdminLayout.jsx
+++ src/components/Admin/layout/AdminLayout.jsx
@@ -18,7 +18,7 @@ function AdminLayout() {
 
   return (
     <>
-      <div className="lg:flex-col lg:items-center lg:justify-between p-5">
+      <div className="lg:flex-col lg:items-center lg:justify-between p-5 ">
         <div className="min-w-0 flex-1">
           <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
             Job Portal
@@ -29,13 +29,13 @@ function AdminLayout() {
 
 
           <ul className='mt-10 flex gap-x-6'>
-          <Link to={'/admin'}>
-            <li
-              className={`uppercase cursor-pointer font-semibold px-3 py-2 transition-transform duration-300 ease-in-out transform ${navlink === 'home' ? 'bg-blue-500 text-white' : 'hover:scale-105'}`}
-              onClick={() => { setNavlink('home'); }}
-            >
-              Home
-            </li></Link>
+            <Link to={'/admin'}>
+              <li
+                className={`uppercase cursor-pointer font-semibold px-3 py-2 transition-transform duration-300 ease-in-out transform ${navlink === 'home' ? 'bg-blue-500 text-white' : 'hover:scale-105'}`}
+                onClick={() => { setNavlink('home'); }}
+              >
+                Home
+              </li></Link>
             <Link to={'addjobs'}>
               <li
                 className={`uppercase cursor-pointer font-semibold px-3 py-2 transition-transform duration-300 ease-in-out transform ${navlink === 'add-jobs' ? 'bg-blue-500 text-white' : 'hover:scale-105'}`}
@@ -67,9 +67,11 @@ function AdminLayout() {
       <div className="bg-gray-50 p-6">
         <Outlet />
       </div>
-      <footer className=" text-black p-10 bg-gray-100 text-center">
+      <footer className="text-black p-10 bg-gray-100 text-center fixed bottom-0 left-0 w-full">
         <p>&copy; 2024 Job Portal. All rights reserved.</p>
       </footer>
+
+
     </>
   )
 }
