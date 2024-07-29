6904e80370359586fbeaa62a2f37885440deb091
bharathkumar <bharathkumar100q@gmail.com>
2024-07-29 16:52:49 +0530

    backend started

--- src/components/Admin/layout/AdminLayout.jsx
+++ src/components/Admin/layout/AdminLayout.jsx
@@ -18,61 +18,64 @@ function AdminLayout() {
 
   return (
     <>
-      <div className="lg:flex-col lg:items-center lg:justify-between p-5 ">
-        <div className="min-w-0 flex-1">
-          <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
-            Job Portal
-          </h2>
-        </div>
+      
+        <div className="lg:flex-col lg:items-center lg:justify-between p-5 ">
+          <div className="min-w-0 flex-1">
+            <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
+              Job Portal
+            </h2>
+          </div>
 
-        <div className=" flex  sm:mt-0 sm:flex-row sm:flex-wrap sm:space-x-6">
+          <div className=" flex  sm:mt-0 sm:flex-row sm:flex-wrap sm:space-x-6">
 
+        
+            <ul className='mt-10 flex gap-x-6'>
+              <Link to={'/admin'}>
+                <li
+                  className={`uppercase cursor-pointer font-semibold px-3 py-2 transition-transform duration-300 ease-in-out transform ${navlink === 'home' ? 'bg-blue-500 text-white' : 'hover:scale-105'}`}
+                  onClick={() => { setNavlink('home'); }}
+                >
+                  Home
+                </li></Link>
+              <Link to={'addjobs'}>
+                <li
+                  className={`uppercase cursor-pointer font-semibold px-3 py-2 transition-transform duration-300 ease-in-out transform ${navlink === 'add-jobs' ? 'bg-blue-500 text-white' : 'hover:scale-105'}`}
+                  onClick={() => { setNavlink('add-jobs'); }}
+                >
+                  Add Jobs
+                </li>
+              </Link>
 
-          <ul className='mt-10 flex gap-x-6'>
-            <Link to={'/admin'}>
               <li
-                className={`uppercase cursor-pointer font-semibold px-3 py-2 transition-transform duration-300 ease-in-out transform ${navlink === 'home' ? 'bg-blue-500 text-white' : 'hover:scale-105'}`}
-                onClick={() => { setNavlink('home'); }}
+                className={`uppercase cursor-pointer font-semibold px-3 py-2 transition-transform duration-300 ease-in-out transform ${navlink === 'view-candidates' ? 'bg-blue-500 text-white' : 'hover:scale-105'}`}
+                onClick={() => { setNavlink('view-candidates'); }}
               >
-                Home
-              </li></Link>
-            <Link to={'addjobs'}>
-              <li
-                className={`uppercase cursor-pointer font-semibold px-3 py-2 transition-transform duration-300 ease-in-out transform ${navlink === 'add-jobs' ? 'bg-blue-500 text-white' : 'hover:scale-105'}`}
-                onClick={() => { setNavlink('add-jobs'); }}
-              >
-                Add Jobs
+                View Candidates
               </li>
-            </Link>
+            </ul>
 
-            <li
-              className={`uppercase cursor-pointer font-semibold px-3 py-2 transition-transform duration-300 ease-in-out transform ${navlink === 'view-candidates' ? 'bg-blue-500 text-white' : 'hover:scale-105'}`}
-              onClick={() => { setNavlink('view-candidates'); }}
-            >
-              View Candidates
-            </li>
-          </ul>
-
-          {/* <Link to={'addjobs'}>
+            {/* <Link to={'addjobs'}>
               {p === 'addjobs' ? (<></>) : (<>
                 <button className='mt-6 bg-blue-700 px-5 text-xl text-white py-2 rounded-lg'>Add Job</button>
               </>)}
 
             </Link> */}
 
+          </div>
+
+
         </div>
-
-
-      </div>
-      <div className="bg-gray-50 p-6">
-        <Outlet />
-      </div>
-      <footer className="text-black p-10 bg-gray-100 text-center  bottom-0 left-0 w-full">
-        <p>&copy; 2024 Job Portal. All rights reserved.</p>
-      </footer>
+        <div className="bg-gray-50 p-6 min-h-screen">
+          <Outlet />
+        </div>
+      
+        <footer className="text-black p-10 bg-gray-100 text-center ">
+          <p>&copy; 2024 Job Portal. All rights reserved.</p>
+        </footer>
 
 
     </>
+
   )
 }
 
